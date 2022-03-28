f14ed08ac21b75dc91e27fd6877db1cbf1b833a0
Dariusz Okonski <darek200180@gmail.com>
2022-03-28 11:37:25 +0100

    add config.js file

---
+++ src/firebase/config.js
@@ -0,0 +1,21 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyC1IXpt5pYo6PlxJwMvROpRjyraEXi7-2Y",
+    authDomain: "cooking-ninja-site-v2.firebaseapp.com",
+    projectId: "cooking-ninja-site-v2",
+    storageBucket: "cooking-ninja-site-v2.appspot.com",
+    messagingSenderId: "21650820430",
+    appId: "1:21650820430:web:767970ccdd6140be50621f"
+};
+
+// init firebase
+firebase.initializeApp(firebaseConfig);
+
+// init services
+const projectFirestore = firebase.firestore();
+
+export {
+    projectFirestore,
+}
