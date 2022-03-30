2dab0534e80cc45c1dfbdf73bf83db24e93b1594
Dariusz Okonski <darek200180@gmail.com>
2022-03-30 16:38:40 +0100

    work on Login component

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,7 +1,42 @@
 import './Login.css'
+import { useState } from 'react';
 
 export default function Login() {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    console.log('handleSubmit');
+  }
+
   return (
-    <div className='login'>Login</div>
+    <div className='login'>
+      <form onSubmit={handleSubmit} className="login-form">
+        <h3>Login</h3>
+        <label>
+          <span>email:</span>
+          <input 
+            type="email" 
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+        </label>
+
+        <label>
+          <span>password:</span>
+          <input 
+            type="password" 
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </label>
+
+        <button className="btn">Login</button>
+      </form>
+    </div>
   )
 }
